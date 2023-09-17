85f0d18e667b9b389c18c71a514c9656a1df86f3
Ricardo Noronha de Menezes <ricardo.noronha@outlook.com>
2023-09-17 14:14:50 -0300

    tela principal

--- src/paginas/Principal/index.js
+++ src/paginas/Principal/index.js
@@ -1,28 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, Image, TouchableOpacity, TextInput, Alert, ScrollView } from 'react-native';
 import estilos from './estilos';
+import { buscarUsuario } from '../services/api';
+import { useIsFocused } from '@react-navigation/native';
 
 export default function Principal({ navigation }) {
     const [nomeUsuario, setNomeUsuario] = useState('');
     const [usuario, setUsuario] = useState({});
+    const [buscando, setBuscando] = useState(false);
+
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        if (nomeUsuario) {
+            pesquisarUsuario();
+        }
+
+    }, [isFocused]);
+
+    async function pesquisarUsuario() {
+        try {
+            setBuscando(true);
+            const usuario = await buscarUsuario(nomeUsuario);
+            setUsuario(usuario);
+            setNomeUsuario("");
+
+        }
+        catch (error) {
+            Alert.alert(error.message);
+        }
+        finally {
+            setBuscando(false);
+        }
+
+    }
+
+
 
     return (
         <ScrollView>
             <View style={estilos.container}>
-                <>
+                {usuario?.name && <>
                     <View style={estilos.fundo} />
                     <View style={estilos.imagemArea}>
-                        <Image source={{ uri: 'https://avatars.githubusercontent.com/u/54721131?v=4' }} style={estilos.imagem} />
+                        <Image source={{ uri: usuario.avatar_url }} style={estilos.imagem} />
                     </View>
-                    <Text style={estilos.textoNome}>Nome do usuario</Text>
-                    <Text style={estilos.textoEmail}>Email do usuario</Text>
+                    <Text style={estilos.textoNome}>{usuario.name}</Text>
+                    <Text style={estilos.textoEmail}>{usuario.email}</Text>
                     <View style={estilos.seguidoresArea}>
                         <View style={estilos.seguidores}>
-                            <Text style={estilos.seguidoresNumero}>30</Text>
+                            <Text style={estilos.seguidoresNumero}>{usuario.followers}</Text>
                             <Text style={estilos.seguidoresTexto}>Seguidores</Text>
                         </View>
                         <View style={estilos.seguidores}>
-                            <Text style={estilos.seguidoresNumero}>40</Text>
+                            <Text style={estilos.seguidoresNumero}>{usuario.following}</Text>
                             <Text style={estilos.seguidoresTexto}>Seguindo</Text>
                         </View>
                     </View>
@@ -31,17 +62,19 @@ export default function Principal({ navigation }) {
                             Ver os repositórios
                         </Text>
                     </TouchableOpacity>
-                </>
+                </>}
 
                 <TextInput
                     placeholder="Busque por um usuário"
                     autoCapitalize="none"
                     style={estilos.entrada}
+                    value={nomeUsuario}
+                    onChangeText={setNomeUsuario}
                 />
 
-                <TouchableOpacity style={estilos.botao}>
+                <TouchableOpacity style={estilos.botao} onPress={pesquisarUsuario} disabled={buscando}>
                     <Text style={estilos.textoBotao}>
-                        Buscar
+                        {buscando ? "Buscando..." : "Buscar"}
                     </Text>
                 </TouchableOpacity>
             </View>
---
+++ src/paginas/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+
+
+
+
+const api = axios.create({
+    baseURL: "http://192.168.157.139:3000"
+});
+
+
+export async function buscarUsuario(nome) {
+
+    if (nome) {
+        const response = await api.get(`users?login=${nome}`);
+        const usuarios = response.data;
+        if (usuarios.length == 1) {
+            return usuarios[0];
+        }
+        else if (usuarios.length == 0) {
+            throw new Error("Nenhum usuário encontrado");
+        }
+        else {
+            throw new Error("Muitos usuários encontrados");
+        }
+
+    }
+
+    throw new Error("Informe o login");
+
+
+
+}
+
